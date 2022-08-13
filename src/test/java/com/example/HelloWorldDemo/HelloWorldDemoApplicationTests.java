21805a85d269d699b28020709aec22bc477516e8
mskhrk7101 <msk19970709@gmail.com>
2022-08-13 18:02:51 +0900

    first commit

---
+++ src/test/java/com/example/HelloWorldDemo/HelloWorldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorldDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
